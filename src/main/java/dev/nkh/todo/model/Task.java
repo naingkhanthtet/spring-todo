ea1891638d6bdfb1188a0b7ef2ae0d968cba096f
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-12-14 19:35:10 +0700

    task model

---
+++ src/main/java/dev/nkh/todo/model/Task.java
@@ -0,0 +1,48 @@
+package dev.nkh.todo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private boolean completed;
+
+    public Task() {
+
+    }
+
+    public Task(boolean completed, String title) {
+        this.completed = completed;
+        this.title = title;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
