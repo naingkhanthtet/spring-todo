a9917f7abfdb0326b5bcb8a52a1f5eaf2f40d0c8
naingkhanthtet <naingkhanthtet15@gmail.com>
2024-12-17 22:17:19 +0700

    enable configuration properties

--- src/main/java/dev/nkh/todo/TodoApplication.java
+++ src/main/java/dev/nkh/todo/TodoApplication.java
@@ -1,8 +1,11 @@
 package dev.nkh.todo;
 
+import dev.nkh.todo.config.ControllerProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(ControllerProperties.class)
 @SpringBootApplication
 public class TodoApplication {
 
